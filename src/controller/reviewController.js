cd78d8f089c3db2484d5671c91e9292bd5de4429
AnishTiwari1531 <anishtiwari1531@gmail.com>
2022-07-11 11:54:43 +0530

    Project 3

--- src/controller/reviewController.js
+++ src/controller/reviewController.js
@@ -1,3 +1,4 @@
+const { default: mongoose } = require("mongoose");
 const booksModel = require("../models/booksModel")
 const reviewModel = require("../models/reviewModel")
 const validate = require("../validation/validation")
@@ -9,30 +10,30 @@ const addReview = async function (req, res) {
         let bookId = req.params.bookId;
         if (bookId) {
             if (!validate.isValidObjectId(bookId)) {
-                return res.status(400).send({ status: false, msg: "bookId is not valid book id please check it" })
+                return res.status(400).send({ status: false, msg: "bookId is not valid book id please check it ❌" })
             }
         }
 
         if (!validate.isValidBody(req.body)) {
-            return res.status(400).send({ status: false, message: "Review body is empty" })
+            return res.status(400).send({ status: false, message: "Review body is empty ❌" })
         }
 
         let { reviewedBy, rating, review } = req.body
 
         if (!validate.isValid(reviewedBy)) {
-            return res.status(400).send({ status: false, message: "reviewedBy is not valid" })
+            return res.status(400).send({ status: false, message: "reviewedBy is not valid ❌" })
         }
 
         if (!validate.isValid(review)) {
-            return res.status(400).send({ status: false, message: "review is not valid" })
+            return res.status(400).send({ status: false, message: "review is not valid ❌" })
         }
 
         if (!validate.isValid(rating)) {
-            return res.status(400).send({ status: false, message: "rating is not valid" })
+            return res.status(400).send({ status: false, message: "rating is not valid ❌" })
         }
 
         if (rating < 1 || rating > 5) {
-            return res.status(400).send({ status: false, msg: "Rating should be in between 1 to 5" })
+            return res.status(400).send({ status: false, msg: "Rating should be in between 1 to 5 ❌" })
 
         }
 
@@ -49,10 +50,10 @@ const addReview = async function (req, res) {
 
             let countUpdate = await booksModel.findOneAndUpdate({ _id: req.params.bookId }, { reviews: ReviewCount })
 
-            return res.status(201).send({ status: true, msg: "Thank you for Reviewing the book", addedReview: review })
+            return res.status(201).send({ status: true, msg: "Thank you for Reviewing the book ✅", addedReview: review })
 
         } else {
-            return res.status(404).send({ status: true, msg: "no such book exist to be review" })
+            return res.status(404).send({ status: true, msg: "no such book exist to be review ❌" })
 
 
         }
@@ -71,32 +72,32 @@ const updateReview = async function (req, res) {
         let reviewId = req.params.reviewId;
         if (bookId) {
             if (!validate.isValidObjectId(bookId)) {
-                return res.status(400).send({ status: false, msg: "bookId is not valid book id please check it" })
+                return res.status(400).send({ status: false, msg: "bookId is not valid book id please check it ❌" })
             }
         }
         let book = await booksModel.findOne({ _id: bookId, isDeleted: false })
         if (!book) {
-            return res.status(404).send({ status: false, message: "book not found with this book id or it may be deleted" })
+            return res.status(404).send({ status: false, message: "book not found with this book id or it may be deleted ❌" })
         }
         if (reviewId) {
             if (!validate.isValidObjectId(reviewId)) {
-                return res.status(400).send({ status: false, msg: "reviewId is not valid book id please check it" })
+                return res.status(400).send({ status: false, msg: "reviewId is not valid book id please check it ❌" })
             }
         }
         let bookReview = await reviewModel.findOne({ _id: reviewId, isDeleted: false })
         if (!bookReview) {
-            return res.status(404).send({ status: false, message: "review not found with this book id or it may be deleted" })
+            return res.status(404).send({ status: false, message: "review not found with this book id or it may be deleted ❌" })
         }
         let data = req.body;
         if (!validate.isValidBody(data)) {
-            return res.status(400).send({ status: false, message: "please provide what you want  to update" })
+            return res.status(400).send({ status: false, message: "please provide what you want  to update ❌" })
         }
         const { review, reviewedBy, rating } = data;
         if (review) bookReview.review = review;
         if (reviewedBy) bookReview.reviewedBy = reviewedBy;
         if (rating) bookReview.rating = rating
         bookReview.save();
-        res.status(200).send({ status: true, message: "updated succesfully", data: bookReview })
+        res.status(200).send({ status: true, message: "updated succesfully ✅", data: bookReview })
     }
     catch (err) {
         res.status(500).send({ status: false, message: err.message })
@@ -114,25 +115,25 @@ const deleteReview = async function (req, res) {
         const review_Id = req.params.reviewId;
 
         if (!validate.isValidObjectId(book_id)) {
-            return res.status(400).send({ status: false, message: "Invalid BookId." })
+            return res.status(400).send({ status: false, message: "Invalid BookId ❌" })
         }
 
         if (!validate.isValidObjectId(review_Id)) {
-            return res.status(400).send({ status: false, message: "Invalid reviewId." })
+            return res.status(400).send({ status: false, message: "Invalid reviewId ❌" })
         }
 
         let checkBook = await booksModel.findById(book_id)
         if (!checkBook) {
-            return res.status(401).send({ status: false, message: "BookId not found" })
+            return res.status(401).send({ status: false, message: "BookId not found ❌" })
         }
 
         let checkReview = await reviewModel.findById(review_Id)
         if (!checkReview) {
-            return res.status(404).send({ status: false, message: "reviewId not found" })
+            return res.status(404).send({ status: false, message: "reviewId not found ❌" })
         }
 
         if (checkBook.isDeleted == true || checkReview.isDeleted == true) {
-            return res.status(400).send({ status: false, message: "can not delete review of deleted Book" })
+            return res.status(400).send({ status: false, message: "can not delete review of deleted Book ❌" })
         }
 
         const deleteReviewDetails = await reviewModel.findOneAndUpdate({_id: review_Id},{ $set: {isDeleted: true,
@@ -143,7 +144,7 @@ const deleteReview = async function (req, res) {
         if (deleteReviewDetails) {
             await booksModel.findOneAndUpdate({ _id: book_id }, { $inc: { reviews: -1 } })
         }
-        return res.status(200).send({ status: true, message: "Review deleted successfully", data: deleteReviewDetails })
+        return res.status(200).send({ status: true, message: "Review deleted successfully ✅", data: deleteReviewDetails })
 
     } catch (err) {
         res.status(500).send({ status: false, Error: err.message })
--- src/routes/route.js
+++ src/routes/route.js
@@ -7,18 +7,27 @@ const middleware = require("../middleware/auth")
 
 
 
-//--------------------------------------------------------------------//
+//----------------------------USER-------------------------------------//
 
 router.post("/register", userController.registerUser)
 router.post("/login", userController.loginUser)
+
+//--------------------------BOOKS---------------------------------------//
+
 router.post("/books", middleware.tokenChecker, bookController.bookCreation)
 router.get("/books", middleware.tokenChecker, bookController.getBooks)
 router.get("/books/:bookId", middleware.tokenChecker, bookController.getBookByParams)
 router.put("/books/:bookId", middleware.tokenChecker, bookController.updateBooks)
 router.delete("/books/:bookId", middleware.tokenChecker, bookController.deleteBookById)
+
+//-----------------------REVIEW-----------------------------------------//
+
 router.post("/books/:bookId/review", reviewController.addReview)
 router.put("/books/:bookId/review/:reviewId", reviewController.updateReview)
 router.delete("/books/:bookId/review/:reviewId", reviewController.deleteReview)
 
 //--------------------------------------------------------------------//
+
 module.exports = router;
+
+//--------------------------------------------------------------------//
